e19e81a51f9460edff1c77e29fdb0dacf102bc87
manish-gill <110728944+manish-gill@users.noreply.github.com>
2024-07-20 22:37:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=f6c45aae`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=f6c45aae`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
